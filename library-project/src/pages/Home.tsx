d9746dac7fd1cc302bb606f35dd6b750beede6d0
Med Marwen Meddeb <marwen.meddeb1812@gmail.com>
2024-02-26 14:12:08 +0100

    feat: Added Home page + design

--- library-project/src/pages/Home.tsx
+++ library-project/src/pages/Home.tsx
@@ -1,12 +1,24 @@
 // Page d'accueil
 
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Home.css';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      <h1>Accueil</h1>
-      {/* Contenu de la page d'accueil */}
+    <div className="home-container">
+      <h1>Bienvenue à la Bibliothèque SupWorld</h1>
+      <p>Découvrez notre vaste collection de livres passionnants.</p>
+      <div className="cta-buttons">
+        <Link to="/search" className="cta-button">Recherche Avancée</Link>
+        <Link to="/search" className="cta-button">Voir les Nouveautés</Link>
+      </div>
+      <p>Vous pouvez également parcourir nos catégories populaires :</p>
+      <ul className="categories-list">
+        <li><Link to="/search?q=science">Science</Link></li>
+        <li><Link to="/search?q=fiction">Fiction</Link></li>
+        <li><Link to="/search?q=mystery">Mystère</Link></li>
+      </ul>
     </div>
   );
 }
